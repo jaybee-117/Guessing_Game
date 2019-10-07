7e45f3e85d3fb7c95a0e887d4860a4fd0b02a4cd
jaybee-117 <unofficialme117@gmail.com>
2019-10-07 18:04:46 +0530

    Rand functionality added to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng
 
 fn main(){
 	println!("Welcome to the guessing game!!\n");
+	let secret_number = thread_rng().gen_range(1,101);
+	println!("The secret number is : {}", secret_number);
 	println!("Enter your guess number:");
 	let mut guess = String::new();
 	io::stdin().read_line(&mut guess)
