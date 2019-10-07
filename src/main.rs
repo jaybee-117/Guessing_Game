5c01661ebd49155482d67243b174f217211f3cc0
jaybee-117 <unofficialme117@gmail.com>
2019-10-07 14:47:03 +0530

    Third commit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::io
 
 fn main(){
 	println!("Welcome to the guessing game!!\n");
-	println!("Enter your guess:");
-	io:: .read_line(&mut guess)
-         .expect("Couldn't read line");
+	println!("Enter your guess number:");
+	let mut guess = String::new();
+	io::stdin().read_line(&mut guess)
+               .expect("Couldn't read line");
+    println!("You guessed : {}", guess);
 }
